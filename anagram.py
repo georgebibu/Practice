8af44a9c3e6f14500626087b0ad202c6c6be48fb
George Bibu <92991300+georgebibu@users.noreply.github.com>
2026-04-08 22:30:14 +0530

    Update anagram.py

--- anagram.py
+++ anagram.py
@@ -1,21 +1,11 @@
 #Given two strings s and t, return true if t is an anagram of s, and false otherwise.
-l=[]
-flag=0
-str1=input("Enter string:")
-str=str1
-str2=input("Enter anagram:")
-if len(str1)!=len(str2):
-    print("Not a valid anagram")
-    flag=1
-else:
-    for i in str2:
-        if i in str1:
-            str1=str1.replace(i,'',1)
-        else:
-            print("Not a valid anagram")
-            flag=1
-            break
-if flag==0:
-    print(str2+" is a valid anagram of "+str)
-    
-
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        a = {}
+        b = {}
+        for i in range(len(s)):
+            a[s[i]] = 1 + a.get(s[i], 0)
+            b[t[i]] = 1 + b.get(t[i], 0)
+        return a==b
