5c1f9716f612e2e2965da5b724acf004f29a710c
George Bibu <92991300+georgebibu@users.noreply.github.com>
2026-04-09 15:13:23 +0530

    Update twosum.py

--- twosum.py
+++ twosum.py
@@ -1,18 +1,8 @@
 #Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
-flag=0
-nums=[]
-n=int(input("Enter length of list:"))
-for i in range(n):
-    x=int(input("Enter element:"))
-    nums.append(x)
-t=int(input("Enter target:"))
-for i in range(n):
-    for j in range(i+1,n):
-        if nums[i]+nums[j]==t:
-            flag=1
-            print(f"Elements at indices {i} and {j} add upto {t}")
-            break
-    if flag==1:
-        break
-if flag==0:
-    print("No pair of elements in the list adds upto the target")
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        k = {}
+        for i in range(len(nums)):
+            if target - nums[i] in k:
+                return [k[target - nums[i]], i]
+            k[nums[i]] = i
