8674d3d0a6c72cf3c4e5ad1e603f7d783286b0bd
George Bibu <92991300+georgebibu@users.noreply.github.com>
2026-04-10 15:25:48 +0530

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -1,14 +1,17 @@
-str=input("Enter a string:")
-str=str.lower()
-rev=""
-for i in str:
-    if i.isalnum() is False:
-        str=str.replace(i,'',1)
-    if i.isnumeric():
-        str=str.replace(i,'',1)
-for i in range(len(str)-1,-1,-1):
-    rev=rev+str[i]
-if str==rev:
-    print(str," is a palindrome")
-else:
-    print(str," is not a palindrome")
+#Given a string s, return true if it is a palindrome, otherwise return false.
+#A palindrome is a string that reads the same forward and backward. It is also case-insensitive and ignores all non-alphanumeric characters.
+#Note: Alphanumeric characters consist of letters (A-Z, a-z) and numbers (0-9).
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        i = 0
+        j = len(s) - 1
+        while i < j:
+            while not s[i].isalnum() and i < j:
+                i += 1
+            while not s[j].isalnum() and j > i:
+                j -= 1
+            if s[i].lower() != s[j].lower():
+                return False
+            i += 1
+            j -= 1
+        return True
