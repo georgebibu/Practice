8537817fcc3cec9fc17eeacb443d5bedb37755e2
George Bibu <92991300+georgebibu@users.noreply.github.com>
2026-04-14 15:05:29 +0530

    Update container.py

--- container.py
+++ container.py
@@ -1,15 +1,13 @@
-n=int(input("Enter length:"))
-height=[]
-a=0
-for i in range(n):
-    x=int(input("Enter height:"))
-    height.append(x)
-for i in range(n):
-    for j in range(i+1,n):
-        if height[i]<height[j]:
-            temp=height[i]*(j-i)
-        else:
-            temp=height[j]*(j-i)
-        if temp>a:
-            a=temp
-print(f"Maximum area is:{a}")
+class Solution:
+    def maxArea(self, heights: List[int]) -> int:
+        i = 0
+        j = len(heights) - 1
+        max_a = 0
+        while i < j:
+            area = min(heights[i], heights[j]) * (j - i)
+            max_a = max(area, max_a)
+            if heights[i] <= heights[j]:
+                i += 1
+            else:
+                j -= 1
+        return max_a
