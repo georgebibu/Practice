7f0eaf9a3f88e7f3f9cccfefbf6767418f888308
George Bibu <92991300+georgebibu@users.noreply.github.com>
2026-04-15 18:10:05 +0530

    Update binarysearch.py

--- binarysearch.py
+++ binarysearch.py
@@ -1,3 +1,6 @@
+#You are given an array of distinct integers nums, sorted in ascending order, and an integer target.
+#Implement a function to search for target within nums. If it exists, then return its index, otherwise, return -1.
+#Your solution must run in O(logn) time.
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
         high=len(nums)-1
