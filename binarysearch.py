0e75d23ecec8df84615b579ec407940d0b90b298
George Bibu <92991300+georgebibu@users.noreply.github.com>
2026-04-15 18:09:21 +0530

    Update binarysearch.py

--- binarysearch.py
+++ binarysearch.py
@@ -1,16 +1,16 @@
-nums=[-1,0,2,4,6,8]
-target=0
-high=len(nums)-1
-low=0
-mid=(low+high)//2
-while(low<=high):
-    if nums[mid]==target:
-        print(f"Found at {mid}")
-        exit()
-    elif target>nums[mid]:
-        low=mid+1
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        high=len(nums)-1
+        low=0
         mid=(low+high)//2
-    else:
-        high=mid-1
-        mid=(low+high)//2
-print("Not found")
+        while(low<=high):
+            if nums[mid]==target:
+                return mid
+            elif target>nums[mid]:
+                low=mid+1
+                mid=(low+high)//2
+            else:
+                high=mid-1
+                mid=(low+high)//2
+        return -1
+        
