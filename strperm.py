bbbc742ef7e98d5be19e64524c0f3bcf0484f562
George Bibu <92991300+georgebibu@users.noreply.github.com>
2026-04-22 12:49:18 +0530

    Update strperm.py

--- strperm.py
+++ strperm.py
@@ -1,22 +1,34 @@
-s1="dca"
-s2="dcda"
-s1=list(s1)
-original=s1.copy()
-s2=list(s2)
-n=len(s2)
-window=len(s1)
-start,end=0,window
-while end<=n:
-    for i in range(start,end):
-        if s2[i] in s1:
-            print(s1)
-            s1.remove(s2[i])
-            if s1==[]:
-                print("True")
-                exit()
-        else:
-            s1=original.copy()
-            start+=1
-            end+=1
-            break
-print("False")
+#You are given two strings s1 and s2.
+#Return true if s2 contains a permutation of s1, or false otherwise. That means if a permutation of s1 exists as a substring of s2, then return true.
+#Both strings only contain lowercase letters.
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s1) > len(s2):
+            return False
+        s1_freq, s2_freq = [0]*26, [0]*26
+        for i in range(len(s1)):
+            s1_freq[ord(s1[i]) -  ord('a')] += 1
+            s2_freq[ord(s2[i]) -  ord('a')] += 1
+        matches = 0
+        for i in range(26):
+            if s1_freq[i] == s2_freq[i]:
+                matches += 1
+        l = 0
+        for r in range(len(s1), len(s2)):
+            if matches == 26:
+                return True
+            index = ord(s2[r]) - ord('a')
+            s2_freq[index] += 1
+            if s1_freq[index] == s2_freq[index]:
+                matches += 1
+            elif s1_freq[index] + 1 == s2_freq[index]:
+                matches -= 1
+            
+            index = ord(s2[l]) - ord('a')
+            s2_freq[index] -= 1
+            if s1_freq[index] == s2_freq[index]:
+                matches += 1
+            elif s1_freq[index] - 1 == s2_freq[index]:
+                matches -= 1
+            l += 1
+        return matches == 26
