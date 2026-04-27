e49aa412a0e92cd8110bc0919a7da746dc29eecc
George Bibu <92991300+georgebibu@users.noreply.github.com>
2026-04-27 16:08:48 +0530

    Update matrixbinsearch.py

--- matrixbinsearch.py
+++ matrixbinsearch.py
@@ -1,17 +1,34 @@
-matrix=[[1,3,5,7],[10,11,16,20],[23,30,34,60]]
-target=10
-for nums in matrix:
-    high=len(nums)-1
-    low=0
-    mid=(low+high)//2
-    while(low<=high):
-        if nums[mid]==target:
-            print(f"Found at {mid} in {nums}")
-            exit(0)
-        elif target>nums[mid]:
-            low=mid+1
-            mid=(low+high)//2
-        else:
-            high=mid-1
-            mid=(low+high)//2
-print("Not found")
+#You are given an m x n 2-D integer array matrix and an integer target.
+#Each row in matrix is sorted in non-decreasing order.
+#The first integer of every row is greater than the last integer of the previous row.
+#Return true if target exists within matrix or false otherwise.
+#Can you write a solution that runs in O(log(m * n)) time?
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        rows, cols = len(matrix), len(matrix[0])
+        low = 0
+        high = rows - 1
+        mid = (low + high) // 2
+        while low <= high:
+            if target > matrix[mid][-1]:
+                low = mid + 1
+                mid = (low + high) // 2
+            elif target < matrix[mid][0]:
+                high = mid - 1
+                mid = (low + high) // 2
+            else:
+                row = mid
+                low = 0
+                high = cols - 1
+                mid = (low + high) // 2
+                while low <= high:
+                    if target == matrix[row][mid]:
+                        return True
+                    elif target > matrix[row][mid]:
+                        low = mid + 1
+                        mid = (low + high) // 2
+                    else:
+                        high = mid - 1
+                        mid = (low + high) // 2
+                return False
+        return False
