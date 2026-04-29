822f92b195f2d036511a6641deaee5891d3f1a2b
George Bibu <92991300+georgebibu@users.noreply.github.com>
2026-04-29 14:58:01 +0530

    Update revlink.py

--- revlink.py
+++ revlink.py
@@ -1,29 +1,17 @@
-class Node:
-    def __init__(self,data):
-        self.data=data
-        self.next=None
-ptr,head,tail,after,before=None,None,None,None,None
-n=int(input("Enter length of list:"))
-for i in range(n):
-    data=int(input("Enter data:"))
-    p=Node(data)
-    if head==None:
-        head,tail=p,p
-    else:
-        tail.next=p
-        tail=p
-ptr=head
-tail=head
-after=ptr.next
-while ptr!=None:
-    ptr.next=before
-    before=ptr
-    head=ptr
-    ptr=after
-    if after==None:
-        break
-    after=after.next
-ptr=head
-while ptr!=None:
-    print(ptr.data)
-    ptr=ptr.next
+#Given the beginning of a singly linked list head, reverse the list, and return the new beginning of the list.
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev = None
+        curr = head
+        while curr != None:
+            nxt = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nxt
+        return prev
