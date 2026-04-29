237dca9985d8ea5f8ffacb1710eebcafa313a207
George Bibu <92991300+georgebibu@users.noreply.github.com>
2026-04-29 15:12:30 +0530

    Update mergelink.py

--- mergelink.py
+++ mergelink.py
@@ -1,90 +1,30 @@
-class Node:
-    def __init__(self,data):
-        self.data=data
-        self.next=None
-n1=int(input("Enter length of list 1:"))
-list1,list2,tail1,tail2=None,None,None,None
-for i in range(n1):
-    data=int(input("Enter data:"))
-    new_node=Node(data)
-    new_node.next=None
-    if(list1==None):
-        list1=new_node
-        tail1=new_node
-    else:
-        tail1.next=new_node
-        tail1=new_node
-n2=int(input("Enter length of list 2:"))
-for i in range(n2):
-    data=int(input("Enter data:"))
-    new_node=Node(data)
-    new_node.next=None
-    if(list2==None):
-        list2=new_node
-        tail2=new_node
-    else:
-        tail2.next=new_node
-        tail2=new_node
-head=None
-tail=None
-p1=list1
-p2=list2
-while(p1!=None and p2!=None):
-    if(p1.data<p2.data):
-        new_node=Node(p1.data)
-        if(head==None):
-            head=new_node
-            tail=new_node
-        else:
-            tail.next=new_node
-            tail=new_node
-        p1=p1.next
-    elif(p2.data<p1.data):
-        new_node=Node(p2.data)
-        if(head==None):
-            head=new_node
-            tail=new_node
-        else:
-            tail.next=new_node
-            tail=new_node
-        p2=p2.next
-    else:
-        new_node=Node(p1.data)
-        if(head==None):
-            head=new_node
-            tail=new_node
-        else:
-            tail.next=new_node
-            tail=new_node
-        new_node=Node(p2.data)
-        if(head==None):
-            head=new_node
-            tail=new_node
-        else:
-            tail.next=new_node
-            tail=new_node
-        p1=p1.next
-        p2=p2.next
-if(p1!=None):
-    while(p1!=None):
-        new_node=Node(p1.data)
-        if(head==None):
-            head=new_node
-            tail=new_node
-        else:
-            tail.next=new_node
-            tail=new_node
-        p1=p1.next
-else:
-    while(p2!=None):
-        new_node=Node(p2.data)
-        if(head==None):
-            head=new_node
-            tail=new_node
-        else:
-            tail.next=new_node
-            tail=new_node
-        p2=p2.next
-while(head!=None):
-    print(head.data,end=" ")
-    head=head.next
+#You are given the heads of two sorted linked lists list1 and list2.
+#Merge the two lists into one sorted linked list and return the head of the new sorted linked list.
+#The new list should be made up of nodes from list1 and list2.
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode()
+        tail = dummy
+        while list1 and list2:
+            if list1.val <= list2.val:
+                tail.next = list1
+                list1 = list1.next
+            else:
+                tail.next = list2
+                list2 = list2.next
+            tail = tail.next
+        while list1:
+            tail.next = list1
+            list1 = list1.next
+            tail = tail.next
+        while list2:
+            tail.next = list2
+            list2 = list2.next
+            tail = tail.next
+        return dummy.next
