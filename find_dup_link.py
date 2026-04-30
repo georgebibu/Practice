13e60b1d453596f084a4e16dc4ac1690e0b123c5
George Bibu <92991300+georgebibu@users.noreply.github.com>
2026-04-30 11:34:35 +0530

    Create find_dup_link.py

---
+++ find_dup_link.py
@@ -0,0 +1,15 @@
+#You are given an array of integers nums containing n + 1 integers. Each integer in nums is in the range [1, n] inclusive.
+#Every integer appears exactly once, except for one integer which appears two or more times. Return the integer that appears more than once.
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        slow = fast = 0
+        while True:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+            if slow == fast:
+                break
+        slow2 = 0
+        while slow != slow2:
+            slow2 = nums[slow2]
+            slow = nums[slow]
+        return slow
