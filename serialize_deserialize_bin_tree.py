a782588bb77b2cd619c5b7c23694f3e19271d555
George Bibu <92991300+georgebibu@users.noreply.github.com>
2026-05-06 10:21:07 +0530

    Create serialize_deserialize_bin_tree.py

---
+++ serialize_deserialize_bin_tree.py
@@ -0,0 +1,40 @@
+#Implement an algorithm to serialize and deserialize a binary tree.
+#Serialization is the process of converting an in-memory structure into a sequence of bits so that it can be stored or sent across a network to be reconstructed later in another computer environment.
+#You just need to ensure that a binary tree can be serialized to a string and this string can be deserialized to the original tree structure.
+#There is no additional restriction on how your serialization/deserialization algorithm should work.
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Codec:
+    
+    # Encodes a tree to a single string.
+    def serialize(self, root: Optional[TreeNode]) -> str:
+        res = []
+        def dfs(node):
+            if not node:
+                res.append("N")
+                return
+            res.append(str(node.val))
+            dfs(node.left)
+            dfs(node.right)
+        dfs(root)
+        return ",".join(res)
+        
+    # Decodes your encoded data to tree.
+    def deserialize(self, data: str) -> Optional[TreeNode]:
+        vals = data.split(",")
+        self.i = 0
+        def dfs():
+            if vals[self.i] == "N":
+                self.i += 1
+                return None
+            node = TreeNode(int(vals[self.i]))
+            self.i += 1
+            node.left = dfs()
+            node.right = dfs()
+            return node
+        return dfs()
